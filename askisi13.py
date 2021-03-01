0508bc4502d96e65da6451fdb2001d7dba0f8494
vassilikikrg <78561945+vassilikikrg@users.noreply.github.com>
2021-03-01 13:47:02 +0200

    Update askisi13.py
    Write code in Python that takes an ASCII text file as input, splits it into words, and displays word pairs whose total character length is exactly 20. Each pair leaves the set and the program ends when there are no more pairs.

--- askisi13.py
+++ askisi13.py
@@ -4,31 +4,33 @@ with open(filename) as file_object:
 
     contents= file_object.read()
 
-    #afairw ari8mous kai shmeia stikshs
+    #remove numbers and punctuation
     char_remove=['0','1','2','3','4','5','6','7','8','9','.',',','!','[',']','(',')','-','{','}',';',':','''"''','<','>','/','?','@','#','$','%','^','&','*','_','~',"""'""",'’','“','‘']
     for charact in char_remove:
         contents=contents.replace(charact,' ')
 
     words=contents.split()
 
-    #dymioyrgw dict opoy key=leksh kai value=mhkos lekshs
+    #create dict  where key=word and value=word's length
     d={}
     for word in words:
         word1=word.lstrip()
         d[word1]=len(word1)
 
     flag=0
-    #zeygarwnw lekseis bash mhkous xarakthrwn(=20) kai afou tis zeygarwsw 8etw to mhkos toys iso me 100 wste na mhn xrhsimopoih8oyn se allo zeygari
+    #pair the words based on their length.The sum of their chars must be 20
+    #After the pairing,their length is set to 100 so they are not used in other pairs.
     for i in range (1,11):
         lst=[]
         for k_1,v_1 in d.items():
             if int(v_1)==i:
                 for k_2,v_2 in d.items():
                     if k_1!=k_2 and int(v_1)+int(v_2)==20:
-                        print(k_1.lower(),k_2.lower(),",")
+                        print(k_1.lower(),k_2.lower(),"\n")
                         d[k_1]=100
                         d[k_2]=100
                         flag=1
                         break
+
     if flag==0:
         print('Δεν βρέθηκε ζευγάρι λέξεων')
